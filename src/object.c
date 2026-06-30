100897a89679cb54d0ffb4aa63dc54ad201907ca
sathwikv2005 <sathwikv2005@gmail.com>
2026-06-30 14:21:45 +0530

    add Objects

---
+++ src/object.c
@@ -0,0 +1,26 @@
+#include "object.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include "../include/memory.h"
+#include "engine.h"
+#include "value.h"
+
+#define ALLOCATE_OBJ(type, objectType) \
+    (type*)allocateObject(sizeof(type), objectType)
+
+static Obj* allocateObject(size_t size, ObjType type) {
+    Obj* object = (Obj*)reallocate(NULL, 0, size);
+    object->type = type;
+    object->isMarked = !engine.currentGCMark;
+    object->next = engine.objects;
+    engine.objects = object;
+
+#ifdef HELIUM_DEBUG
+    if (GET_DEBUG_LOG_GC())
+        printf("%p allocate %zu for %d\n", (void*)object, size, type);
+#endif
+
+    return object;
+}
