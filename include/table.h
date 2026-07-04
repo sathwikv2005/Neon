c7e424ed6e9069e800725f69643eae3be3a5d161
sathwikv2005 <sathwikv2005@gmail.com>
2026-07-04 15:45:53 +0530

    add hash table

---
+++ include/table.h
@@ -0,0 +1,31 @@
+#ifndef helium_table_h
+#define helium_table_h
+
+#include "common.h"
+#include "value.h"
+
+typedef struct {
+    ObjString* key;
+    Value value;
+} Entry;
+
+typedef struct {
+    int size;   // live entries
+    int count;  // size + tombstones
+    int capacity;
+    int mask;  // mask = capacity-1, used for fast index wrapping. (hash & mask)
+    Entry* entries;
+} Table;
+
+void initTable(Table* table);
+void freeTable(Table* table);
+bool tableSet(Table* table, ObjString* key, Value value);
+void tableAddAll(Table* from, Table* to);
+bool tableGet(Table* table, ObjString* key, Value* value);
+bool tableDelete(Table* table, ObjString* key);
+ObjString* tableFindString(Table* table, const char* chars, int length,
+                           uint32_t hash);
+void markTable(Table* table);
+void tableRemoveWhite(Table* table);
+
+#endif
---
+++ src/table.c
@@ -0,0 +1,161 @@
+#include "table.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/memory.h"
+#include "object.h"
+#include "value.h"
+#include "vm.h"
+
+#define TABLE_MAX_LOAD 0.75
+
+void initTable(Table* table) {
+    table->size = 0;
+    table->count = 0;
+    table->capacity = 0;
+    table->entries = NULL;
+    table->mask = 0;
+}
+
+void freeTable(Table* table) {
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+    initTable(table);
+}
+
+static Entry* findEntry(Entry* entries, int mask, ObjString* key) {
+    // faster than (hash % capacity) since capacity is always a power of 2.
+    // mask = capacity - 1 for fast index wrapping.
+    uint32_t index = key->hash & mask;
+
+    Entry* tombstone = NULL;
+
+    for (;;) {
+        Entry* entry = &entries[index];
+
+        if (entry->key == NULL) {
+            if (IS_NULL(entry->value)) {
+                return tombstone != NULL ? tombstone : entry;
+            } else {
+                if (tombstone == NULL) tombstone = entry;
+            }
+        } else if (entry->key == key) {
+            return entry;
+        }
+        index = (index + 1) & mask;
+    }
+}
+
+bool tableGet(Table* table, ObjString* key, Value* value) {
+    if (table->count == 0) return false;
+
+    Entry* entry = findEntry(table->entries, table->mask, key);
+    if (entry->key == NULL) return false;
+
+    *value = entry->value;
+    return true;
+}
+
+static void adjustCapacity(Table* table, int capacity) {
+    Entry* entries = ALLOCATE(Entry, capacity);
+    int mask = capacity - 1;
+    for (int i = 0; i < capacity; i++) {
+        entries[i].key = NULL;
+        entries[i].value = NULL_VAL;
+    }
+    table->count = 0;
+    table->size = 0;
+    for (int i = 0; i < table->capacity; i++) {
+        Entry* entry = &table->entries[i];
+        if (entry->key == NULL) continue;
+
+        Entry* newEntry = findEntry(entries, mask, entry->key);
+        newEntry->key = entry->key;
+        newEntry->value = entry->value;
+        table->count++;
+        table->size++;
+    }
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+    table->entries = entries;
+    table->capacity = capacity;
+    table->mask = mask;
+}
+
+bool tableSet(Table* table, ObjString* key, Value value) {
+    if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+        int capacity = GROW_CAPACITY(table->capacity);
+        adjustCapacity(table, capacity);
+    }
+
+    Entry* entry = findEntry(table->entries, table->mask, key);
+
+    bool isNewKey = entry->key == NULL;
+    if (isNewKey) {
+        table->size++;
+
+        if (IS_NULL(entry->value)) {
+            table->count++;
+        }
+    }
+
+    entry->key = key;
+    entry->value = value;
+
+    return isNewKey;
+}
+
+void tableAddAll(Table* from, Table* to) {
+    for (int i = 0; i < from->capacity; i++) {
+        Entry* entry = &from->entries[i];
+        if (entry->key == NULL) continue;
+
+        tableSet(to, entry->key, entry->value);
+    }
+}
+
+ObjString* tableFindString(Table* table, const char* chars, int length,
+                           uint32_t hash) {
+    if (table->count == 0) return NULL;
+
+    uint32_t index = hash & table->mask;
+
+    for (;;) {
+        Entry* entry = &table->entries[index];
+        if (entry->key == NULL) {
+            if (IS_NULL(entry->value)) return NULL;
+        } else if (entry->key->length == length && entry->key->hash == hash &&
+                   memcmp(entry->key->chars, chars, length) == 0) {
+            return entry->key;
+        }
+        index = (index + 1) & table->mask;
+    }
+}
+
+bool tableDelete(Table* table, ObjString* key) {
+    if (table->count == 0) return false;
+    Entry* entry = findEntry(table->entries, table->mask, key);
+    if (entry->key == NULL) return false;
+
+    table->size--;
+    entry->key = NULL;
+    entry->value = BOOL_VAL(true);
+
+    return true;
+}
+
+void markTable(Table* table) {
+    for (int i = 0; i < table->capacity; i++) {
+        Entry* entry = &table->entries[i];
+        markObject((Obj*)entry->key);
+        markValue(entry->value);
+    }
+}
+void tableRemoveWhite(Table* table) {
+    for (int i = 0; i < table->capacity; i++) {
+        Entry* entry = &table->entries[i];
+        if (entry->key != NULL &&
+            entry->key->obj.isMarked != vm.currentGCMark) {
+            tableDelete(table, entry->key);
+        }
+    }
+}
