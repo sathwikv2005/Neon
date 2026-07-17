ab39233d66e0b8d824fef5a739bb49e48fad6bfb
sathwikv2005 <sathwikv2005@gmail.com>
2026-07-17 13:15:20 +0530

    fix(database): return false on active clients

--- src/database.c
+++ src/database.c
@@ -31,7 +31,7 @@ Database* loadDatabase(uint8_t id) {
 
 // if no active clients, save database and free the database from memory
 bool unloadDatabase(Database* database) {
-    if (database->clients > 0) return;
+    if (database->clients > 0) return false;
 
     if (!saveDatabase(database)) {
         return false;
