0f0ff221941d03315141281d61ffbf828c03fc1c
sathwikv2005 <sathwikv2005@gmail.com>
2026-07-19 12:10:24 +0530

    feat(logger): add vlog() and logFatal()

--- include/logger.h
+++ include/logger.h
@@ -3,8 +3,11 @@
 
 #include "file.h"
 
-void logFatal(const char* fmt, ...);
 bool initLogger(const char* path);
 void closeLogger(void);
+
+void logFatal(const char* file, int line, const char* fmt, ...);
+
+#define LOG_FATAL(...) logFatal(__FILE__, __LINE__, __VA_ARGS__)
 
 #endif
--- src/logger.c
+++ src/logger.c
@@ -25,4 +25,39 @@ bool initLogger(const char* path) {
 
 void closeLogger(void) { fileClose(&logFile); }
 
-void logFatal(const char* fmt, ...) {}
+static void vlog(LogLevel level, const char* file, int line, const char* fmt,
+                 va_list args) {
+    char buffer[2048];
+    size_t offset = 0;
+
+    time_t now = time(NULL);
+    struct tm tm;
+
+#ifdef _WIN32
+    localtime_s(&tm, &now);
+#else
+    localtime_r(&now, &tm);
+#endif
+
+    offset += snprintf(buffer + offset, sizeof(buffer) - offset,
+                       "[%04d-%02d-%02d %02d:%02d:%02d] %-5s %s:%d: ",
+                       tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour,
+                       tm.tm_min, tm.tm_sec, levelNames[level], file, line);
+
+    offset += vsnprintf(buffer + offset, sizeof(buffer) - offset, fmt, args);
+
+    if (offset >= sizeof(buffer) - 2) {
+        offset = sizeof(buffer) - 2;
+    }
+
+    buffer[offset++] = '\n';
+
+    fileWrite(&logFile, buffer, offset);
+}
+
+void logFatal(const char* file, int line, const char* fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vlog(LOG_FATAL, file, line, fmt, args);
+    va_end(args);
+}
